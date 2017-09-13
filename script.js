a08ad75484a9197e1844e7b474a41fb3b8fc5ecb
Alexandre Claveau <a.claveau29@gmail.com>
2017-09-13 18:59:29 +0200

    Game over and Score

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ window.onload = function () {
     var applee;
     var widthInBlocks = canvasWidth / blockSize;
     var heightInBlocks = canvasHeight / blockSize;
+    var score;
 
     init();
 
@@ -21,7 +22,28 @@ window.onload = function () {
         ctx = canvas.getContext('2d');
         snakee = new Snake([[6, 4], [5, 4], [4, 4], [3, 4], [2, 4]], "right");
         applee = new Apple([10, 10]);
+        score = 0;
         refreshcanvas();
+    }
+
+    function gameOver() {
+        ctx.save();
+        ctx.fillText("Game Over", 5, 15);
+        ctx.fillText("Appuyer sur la touche Espace pour rejouer", 5, 30);
+        ctx.restore();
+    }
+
+    function restart(){
+        snakee = new Snake([[6, 4], [5, 4], [4, 4], [3, 4], [2, 4]], "right");
+        applee = new Apple([10, 10]);
+        score = 0;
+        refreshcanvas();
+    }
+
+    function drawScore(){
+        ctx.save();
+        ctx.fillText(score.toString(), 5, canvasHeight-5);
+        ctx.restore();
     }
 
     function drawBlock(ctx, position) {
@@ -35,9 +57,10 @@ window.onload = function () {
         snakee.advance();
 
         if (snakee.checkCollision()) {
-            //Game Over
+            gameOver();
         } else {
             if (snakee.isEatingApple(applee)) {
+                score++;
                 snakee.ateApple = true;
                 //le serpent à mangé la pomme
                 do {
@@ -47,6 +70,7 @@ window.onload = function () {
             ctx.clearRect(0, 0, canvasWidth, canvasHeight);
             snakee.draw();
             applee.draw();
+            drawScore();
             setTimeout(refreshcanvas, delay);
         }
     }
@@ -192,6 +216,9 @@ window.onload = function () {
             case 40:
                 newDirection = "down";
                 break;
+            case 32:
+                restart();
+                return;
             default:
                 return;
         }
